939e6f5357023990b8044063176f51668ec45cdb
0UMI0 <114157034+0UMI0@users.noreply.github.com>
2023-04-19 00:25:13 +0100

    Add files via upload

--- scripts/contactUsValidation.js
+++ scripts/contactUsValidation.js
@@ -3,7 +3,7 @@ const form = document.querySelector('.ContactUs');
 const name = document.getElementById('user-name');
 const email = document.getElementById('emails');
 const member = document.getElementById('member');
-const non_member = document.getElementById('non_member');
+const nonMember = document.getElementById('non-member');
 const comments = document.getElementById('comments');
 
 // Add an event listener for the form submit button
@@ -11,7 +11,7 @@ form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent default form submission behavior
     
     // Check if all fields are filled in
-    if (name.value === '' || email.value === '' || (member.checked === false && non_member.checked === false) || comments.value === '') {
+    if (name.value === '' || email.value === '' || (member.checked === false && non-member.checked === false) || comments.value === '') {
         alert('Please fill in all fields.'); // Display an alert message if any field is empty
     } else {
         alert('Thank you! The society will contact you soon.'); // Display a success message if all fields are filled in
